00914ef8e35e6048fac942712d002fcccc4ecba2
Vachek-hooves <vachek0602@gmail.com>
2024-09-11 22:40:30 +0300

    build data set for hard level

--- data/quiz_data.js
+++ data/quiz_data.js
@@ -1,5 +1,405 @@
 export const easyLevel = [];
-export const hardLevel = [];
+export const hardLevel = [
+  {
+    active: true,
+    score: 0,
+    id: 'l1',
+    questions: [
+      {
+        question: "This bird is the largest bird on Earth.",
+        correctAnswer: "Ostrich",
+        options: ["Ostrich", "Pelican", "Albatross", "Swan", "Eagle", "Vulture"]
+      },
+      {
+        question: "This bird has the largest wingspan of any living bird.",
+        correctAnswer: "Albatross",
+        options: ["Swan", "Vulture", "Flamingo", "Albatross", "Eagle", "Harpy Eagle"]
+      },
+      {
+        question: "This bird is known as the largest raptor in North America.",
+        correctAnswer: "Bald Eagle",
+        options: ["Bald Eagle", "Golden Eagle", "Harpy Eagle", "Osprey", "Peregrine Falcon"]
+      },
+      {
+        question: "This bird is known for its large size and long legs and can run at high speeds.",
+        correctAnswer: "Ostrich",
+        options: ["Penguin", "Kiwi", "Ostrich", "Kookaburra", "Emu", "Cassowary"]
+      },
+      {
+        question: "This bird is the tallest flying bird in North America.",
+        correctAnswer: "Whooping Crane",
+        options: ["Sandhill Crane", "Whooping Crane", "Bald Eagle", "Golden Eagle", "Pelican", "Vulture"]
+      },
+      {
+        question: "This large bird with a long neck is often seen in wetlands and has a beautiful display during courtship.",
+        correctAnswer: "Swan",
+        options: ["Pelican", "Heron", "Swan", "Crane", "Flamingo", "Ostrich"]
+      },
+      {
+        question: "This bird is the largest of all flightless birds native to Australia.",
+        correctAnswer: "Cassowary",
+        options: ["Penguin", "Cassowary", "Kiwi", "Emu", "Ostrich", "Kookaburra"]
+      },
+      {
+        question: "This bird, known for its impressive wingspan, is often found in the Southern Hemisphere and is famous for its gliding flight.",
+        correctAnswer: "Albatross",
+        options: ["Harpy Eagle", "Vulture", "Albatross", "Peregrine Falcon", "Golden Eagle", "Osprey"]
+      },
+      {
+        question: "This large, scavenging bird is known for its distinctive bald head and can be seen in both North and South America.",
+        correctAnswer: "Turkey Vulture",
+        options: ["Turkey Vulture", "Bald Eagle", "Golden Eagle", "Osprey", "Harpy Eagle", "Peregrine Falcon"]
+      },
+      {
+        question: "This bird is known for its massive beak and is commonly found in tropical regions of Africa and Asia.",
+        correctAnswer: "Hornbill",
+        options: ["Pelican", "Hornbill", "Flamingo", "Crane", "Vulture", "Heron"]
+      }
+    ]
+  },
+  {
+    active: true,
+    score: 0,
+    id: 'l2',
+    questions: [
+      {
+        question: "This bird is a well-known migrant that travels from North America to Central and South America for the winter.",
+        correctAnswer: "Blue Jay",
+        options: ["Sparrow", "Blue Jay", "Rook", "Nightingale", "Yellow Canary", "Red-headed Finch"]
+      },
+      {
+        question: "This bird makes a long migration from Africa to Europe for the breeding season.",
+        correctAnswer: "Starlings",
+        options: ["Swallow", "Starlings", "Stork", "Moorhen", "Yellow Canary", "Cuckoo"]
+      },
+      {
+        question: "This bird is known for traveling thousands of miles between the North and South Poles.",
+        correctAnswer: "Albatross",
+        options: ["Pelican", "Albatross", "Vulture", "Swan", "Horned Puffin", "Sparrow"]
+      },
+      {
+        question: "This bird spends its winter in South America and its summer in North America.",
+        correctAnswer: "Barn Swallow",
+        options: ["Barn Swallow", "Swallow", "Red-headed Finch", "Rook", "Yellow Canary", "Nightingale"]
+      },
+      {
+        question: "This bird migrates from Europe to Africa for the winter, known for its long beak.",
+        correctAnswer: "Stork",
+        options: ["Stork", "Moorhen", "Starlings", "Swallow", "Rook", "Pelican"]
+      },
+      {
+        question: "This bird migrates from North America to Central America and is often seen in gardens and parks.",
+        correctAnswer: "Canary",
+        options: ["Nightingale", "Canary", "Sparrow", "Red-headed Finch", "Rook", "Swallow"]
+      },
+      {
+        question: "This bird migrates from the Southern Pole to North America during the summer season.",
+        correctAnswer: "Penguin",
+        options: ["Penguin", "Pelican", "Swan", "Crane", "Yellow Canary", "Blue Jay"]
+      },
+      {
+        question: "This bird migrates from the south to northern forests, where it lives and breeds in the summer.",
+        correctAnswer: "Starlings",
+        options: ["Swallow", "Starlings", "Stork", "Rook", "Sparrow", "Nightingale"]
+      },
+      {
+        question: "This bird is known for its ability to fly across the ocean from Africa to Europe for the winter.",
+        correctAnswer: "Swift",
+        options: ["Swallow", "Swift", "Stork", "Rook", "Sparrow", "Pelican"]
+      },
+      {
+        question: "This bird makes a long migration from North America to South America for the winter.",
+        correctAnswer: "Cuckoo",
+        options: ["Swallow", "Cuckoo", "Rook", "Yellow Canary", "Pelican", "Blue Jay"]
+      }
+    ]
+  },
+  {
+    active: true,
+    score: 0,
+    id: 'l3',
+    questions: [
+      {
+        question: "This bird is known for its vibrant colors and is native to the rainforests of New Guinea",
+        correctAnswer: "Bird of Paradise",
+        options: ["Bird of Paradise", "Golden Pheasant", "Peacock", "Macaw", "Hornbill", "Flamingo"]
+      },
+      {
+        question: "This bird is famous for its striking blue and green feathers and is native to Madagascar",
+        correctAnswer: "Madagascar Crested Ibis",
+        options: ["Madagascar Crested Ibis", "Harpy Eagle", "Great Hornbill", "Blue Jay", "Macaw", "Scarlet Ibis"]
+      },
+      {
+        question: "This bird is known for its long tail feathers and unique courtship dance, found in the rainforests of South America",
+        correctAnswer: "Long-tailed Manakin",
+        options: ["Long-tailed Manakin", "Hummingbird", "Secretary Bird", "Peacock", "Flamingo", "Macaw"]
+      },
+      {
+        question: "This bird, with its distinctive call and bright plumage, is native to New Zealand and is a rare sight",
+        correctAnswer: "Kakapo",
+        options: ["Kakapo", "Kiwi", "Kea", "Penguin", "Takahe", "Ruru"]
+      },
+      {
+        question: "This bird is known for its iridescent feathers and is native to Australia",
+        correctAnswer: "Superb Fairy-wren",
+        options: ["Superb Fairy-wren", "Rainbow Lorikeet", "Kookaburra", "Lyrebird", "Emu", "Cassowary"]
+      },
+      {
+        question: "This bird is distinguished by its unique vocalizations and is native to Madagascar",
+        correctAnswer: "Indri",
+        options: ["Indri", "Hummingbird", "Quetzal", "Hornbill", "Cassowary", "Secretary Bird"]
+      },
+      {
+        question: "This bird is known for its impressive size and striking white plumage, native to South America",
+        correctAnswer: "Andean Condor",
+        options: ["Andean Condor", "Golden Eagle", "Harpy Eagle", "White-tailed Eagle", "Bald Eagle", "Snowy Owl"]
+      },
+      {
+        question: "This bird is recognized for its vibrant red feathers and is native to Central America",
+        correctAnswer: "Scarlet Macaw",
+        options: ["Scarlet Macaw", "Blue-and-yellow Macaw", "Hyacinth Macaw", "Green-winged Macaw", "Great Hornbill", "Golden Pheasant"]
+      },
+      {
+        question: "This bird is known for its unique feather patterns and is found in the rainforests of Africa",
+        correctAnswer: "African Gray Parrot",
+        options: ["African Gray Parrot", "Madagascar Crested Ibis", "White-cheeked Turaco", "Crowned Crane", "Harpy Eagle", "Secretary Bird"]
+      },
+      {
+        question: "This bird is notable for its bright yellow feathers and is found in the forests of Southeast Asia",
+        correctAnswer: "Golden Pheasant",
+        options: ["Golden Pheasant", "Mandarin Duck", "Peacock", "Resplendent Quetzal", "Green Jay", "Blue Jay"]
+      }
+    ]
+  },
+  {
+    active: true,
+    score: 0,
+    id: 'l4',
+    questions: [
+      {
+        question: "This bird is a symbol of peace and is often depicted holding an olive branch",
+        correctAnswer: "Dove",
+        options: ["Dove", "Eagle", "Hawk", "Sparrow", "Robin", "Crane"]
+      },
+      {
+        question: "This bird is commonly associated with wisdom and is often depicted in ancient Greek mythology",
+        correctAnswer: "Owl",
+        options: ["Owl", "Falcon", "Raven", "Parrot", "Peacock", "Swallow"]
+      },
+      {
+        question: "In Native American cultures, this bird is a symbol of freedom and strength",
+        correctAnswer: "Bald Eagle",
+        options: ["Bald Eagle", "Golden Eagle", "Peregrine Falcon", "Harpy Eagle", "Osprey", "Kestrel"]
+      },
+      {
+        question: "This bird is often associated with good luck and is featured in many Asian cultures",
+        correctAnswer: "Crane",
+        options: ["Crane", "Peacock", "Parrot", "Owl", "Hummingbird", "Swallow"]
+      },
+      {
+        question: "This bird is a symbol of renewal and rebirth in many cultures and myths",
+        correctAnswer: "Phoenix",
+        options: ["Phoenix", "Eagle", "Swan", "Owl", "Peacock", "Crane"]
+      },
+      {
+        question: "In Celtic symbolism, this bird is often associated with protection and the sacred",
+        correctAnswer: "Raven",
+        options: ["Raven", "Dove", "Sparrow", "Hawk", "Owl", "Peacock"]
+      },
+      {
+        question: "This bird is a symbol of love and is often depicted in romantic literature",
+        correctAnswer: "Swan",
+        options: ["Swan", "Dove", "Parrot", "Finch", "Robin", "Blue Jay"]
+      },
+      {
+        question: "This bird is considered a symbol of transformation and is often associated with the mythical phoenix",
+        correctAnswer: "Firebird",
+        options: ["Firebird", "Eagle", "Hawk", "Falcon", "Crane", "Swan"]
+      },
+      {
+        question: "In Hindu mythology, this bird is often associated with the goddess Saraswati and wisdom",
+        correctAnswer: "Swan",
+        options: ["Swan", "Peacock", "Dove", "Eagle", "Raven", "Owl"]
+      },
+      {
+        question: "This bird is often depicted as a symbol of power and royalty in ancient Egyptian culture",
+        correctAnswer: "Falcon",
+        options: ["Falcon", "Hawk", "Eagle", "Owl", "Vulture", "Peacock"]
+      }
+    ]
+  },
+  {
+    active: true,
+    score: 0,
+    id: 'l5',
+    questions: [
+      {
+        question: "This bird is known for its large size and powerful talons, often seen hunting in open fields",
+        correctAnswer: "Bald Eagle",
+        options: ["Bald Eagle", "Hawk", "Falcon", "Owl", "Kestrel", "Harpy Eagle"]
+      },
+      {
+        question: "This bird is known for its incredible speed and is often considered the fastest bird in the world",
+        correctAnswer: "Peregrine Falcon",
+        options: ["Peregrine Falcon", "Eagle", "Hawk", "Kestrel", "Owl", "Vulture"]
+      },
+      {
+        question: "This bird has a distinctive white head and is commonly found near large bodies of water",
+        correctAnswer: "Bald Eagle",
+        options: ["Bald Eagle", "Golden Eagle", "Harpy Eagle", "Osprey", "Kestrel", "Buzzard"]
+      },
+      {
+        question: "This bird is known for its exceptional night vision and silent flight",
+        correctAnswer: "Barn Owl",
+        options: ["Barn Owl", "Golden Eagle", "Peregrine Falcon", "Hawk", "Kestrel", "Osprey"]
+      },
+      {
+        question: "This bird is characterized by its impressive hunting skills and large, curved beak",
+        correctAnswer: "Harpy Eagle",
+        options: ["Harpy Eagle", "Bald Eagle", "Hawk", "Vulture", "Falcon", "Kestrel"]
+      },
+      {
+        question: "This bird is known for its soaring flight and is often seen gliding over mountainous regions",
+        correctAnswer: "Golden Eagle",
+        options: ["Golden Eagle", "Peregrine Falcon", "Kestrel", "Owl", "Hawk", "Vulture"]
+      },
+      {
+        question: "This bird is commonly found near water and is known for its fish-catching abilities",
+        correctAnswer: "Osprey",
+        options: ["Osprey", "Bald Eagle", "Harpy Eagle", "Peregrine Falcon", "Hawk", "Vulture"]
+      },
+      {
+        question: "This bird has a distinctive long tail and is known for its hunting precision",
+        correctAnswer: "Kestrel",
+        options: ["Kestrel", "Eagle", "Hawk", "Falcon", "Owl", "Vulture"]
+      },
+      {
+        question: "This bird is known for its scavenging habits and is often seen in large flocks",
+        correctAnswer: "Vulture",
+        options: ["Vulture", "Peregrine Falcon", "Harpy Eagle", "Golden Eagle", "Kestrel", "Osprey"]
+      },
+      {
+        question: "This bird has powerful talons and a strong beak, making it a skilled predator",
+        correctAnswer: "Hawk",
+        options: ["Hawk", "Eagle", "Falcon", "Owl", "Kestrel", "Vulture"]
+      }
+    ]
+  },
+  {
+    active: true,
+    score: 0,
+    id: 'l6',
+    questions: [
+      {
+        question: "This bird is commonly found in wetlands and is known for its distinctive call and long legs",
+        correctAnswer: "Heron",
+        options: ["Heron", "Sparrow", "Robin", "Canary", "Blue Jay", "Finch"]
+      },
+      {
+        question: "This bird is often seen in desert environments and is known for its ability to go without water for long periods",
+        correctAnswer: "Horned Lark",
+        options: ["Horned Lark", "Sparrow", "Robin", "Blue Jay", "Finch", "Canary"]
+      },
+      {
+        question: "This bird is commonly found in tropical rainforests and is known for its colorful plumage",
+        correctAnswer: "Toucan",
+        options: ["Toucan", "Blue Jay", "Sparrow", "Finch", "Canary", "Robin"]
+      },
+      {
+        question: "This bird is frequently spotted in coastal areas and is known for its ability to dive for fish",
+        correctAnswer: "Cormorant",
+        options: ["Cormorant", "Sparrow", "Robin", "Finch", "Blue Jay", "Canary"]
+      },
+      {
+        question: "This bird is adapted to life in the high mountains and is known for its impressive flying skills",
+        correctAnswer: "Golden Eagle",
+        options: ["Golden Eagle", "Sparrow", "Robin", "Finch", "Blue Jay", "Canary"]
+      },
+      {
+        question: "This bird is a common sight in urban areas and is known for its adaptability to city life",
+        correctAnswer: "Pigeon",
+        options: ["Pigeon", "Sparrow", "Canary", "Robin", "Finch", "Blue Jay"]
+      },
+      {
+        question: "This bird prefers open grasslands and is known for its ability to cover large distances in search of food",
+        correctAnswer: "Meadowlark",
+        options: ["Meadowlark", "Sparrow", "Robin", "Finch", "Blue Jay", "Canary"]
+      },
+      {
+        question: "This bird is commonly found in mangrove swamps and is known for its long, curved bill used for probing mud",
+        correctAnswer: "Spoonbill",
+        options: ["Spoonbill", "Sparrow", "Robin", "Finch", "Blue Jay", "Canary"]
+      },
+      {
+        question: "This bird is known for its ability to live in both freshwater and saltwater environments and is often seen near estuaries",
+        correctAnswer: "Osprey",
+        options: ["Osprey", "Sparrow", "Robin", "Finch", "Blue Jay", "Canary"]
+      },
+      {
+        question: "This bird is commonly found in temperate forests and is known for its distinctive drumming sounds",
+        correctAnswer: "Woodpecker",
+        options: ["Woodpecker", "Sparrow", "Robin", "Finch", "Blue Jay", "Canary"]
+      }
+    ]
+  },
+  {
+    active: true,
+    score: 0,
+    id: 'l7',
+    questions: [
+      {
+        question: "This bird builds a cup-shaped nest using twigs, grass, and mud, and often places it in trees",
+        correctAnswer: "Robin",
+        options: ["Robin", "Sparrow", "Canary", "Blue Jay", "Finch", "Pigeon"]
+      },
+      {
+        question: "This bird constructs a hanging nest that resembles a pouch and often hangs it from trees or shrubs",
+        correctAnswer: "Weaver Bird",
+        options: ["Weaver Bird", "Sparrow", "Robin", "Blue Jay", "Finch", "Canary"]
+      },
+      {
+        question: "This bird creates a large, open nest on the ground, often camouflaged with surrounding vegetation",
+        correctAnswer: "Meadowlark",
+        options: ["Meadowlark", "Sparrow", "Robin", "Finch", "Blue Jay", "Canary"]
+      },
+      {
+        question: "This bird builds an elaborate, intricate nest that looks like a large, tangled ball and is often found in tall trees",
+        correctAnswer: "Bowerbird",
+        options: ["Bowerbird", "Sparrow", "Robin", "Finch", "Blue Jay", "Canary"]
+      },
+      {
+        question: "This bird makes a nest in a cavity or hole in trees and often lines it with soft materials",
+        correctAnswer: "Woodpecker",
+        options: ["Woodpecker", "Sparrow", "Robin", "Finch", "Blue Jay", "Canary"]
+      },
+      {
+        question: "This bird builds a platform-like nest on high cliffs or tall structures, often using large sticks",
+        correctAnswer: "Peregrine Falcon",
+        options: ["Peregrine Falcon", "Sparrow", "Robin", "Finch", "Blue Jay", "Canary"]
+      },
+      {
+        question: "This bird uses mud and twigs to build a bowl-shaped nest, usually found on ledges or in buildings",
+        correctAnswer: "Swallow",
+        options: ["Swallow", "Sparrow", "Robin", "Finch", "Blue Jay", "Canary"]
+      },
+      {
+        question: "This bird constructs a nest out of spider silk, often placing it in hidden locations such as under leaves",
+        correctAnswer: "Long-tailed Tit",
+        options: ["Long-tailed Tit", "Sparrow", "Robin", "Finch", "Blue Jay", "Canary"]
+      },
+      {
+        question: "This bird creates a large, bowl-shaped nest on the ground, often found in open grasslands",
+        correctAnswer: "Killdeer",
+        options: ["Killdeer", "Sparrow", "Robin", "Finch", "Blue Jay", "Canary"]
+      },
+      {
+        question: "This bird builds a nest with a distinctive, intricate structure and often incorporates natural elements like feathers",
+        correctAnswer: "Hummingbird",
+        options: ["Hummingbird", "Sparrow", "Robin", "Finch", "Blue Jay", "Canary"]
+      }
+    ]
+  }
+];
 
 export const ATLAS = [
   {
